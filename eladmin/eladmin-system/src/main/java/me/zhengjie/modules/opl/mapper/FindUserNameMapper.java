5e4d49655884470d4f392461af50bf26741dbfbb
jichao <jichaolsr@sina.com>
2020-10-23 16:46:59 +0800

    用户mapper接口；

---
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/opl/mapper/FindUserNameMapper.java
@@ -0,0 +1,21 @@
+package me.zhengjie.modules.opl.mapper;
+
+import me.zhengjie.modules.security.service.dto.AuthUserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @website https://docs.auauz.net
+ * @Description:
+ * @Auther: chao.ji
+ * @Date: 2020/10/22
+ */
+@Mapper
+public interface FindUserNameMapper {
+
+    Integer findByUserName(String userName);
+
+    String findPasswordByUsername(String userName);
+
+    void updatePasswordByName(AuthUserDto authUserDto);
+
+}
