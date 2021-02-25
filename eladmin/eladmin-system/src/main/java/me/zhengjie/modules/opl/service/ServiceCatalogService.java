b12666ea40afba5346f4f29f182b6af4609ff9fe
ming.cao <15851518625@sina.cn>
2021-02-25 14:04:06 +0800

    更新

--- eladmin/eladmin-system/src/main/java/me/zhengjie/modules/opl/service/ServiceCatalogService.java
+++ eladmin/eladmin-system/src/main/java/me/zhengjie/modules/opl/service/ServiceCatalogService.java
@@ -4,6 +4,7 @@ import me.zhengjie.modules.opl.domain.Pageable;
 import me.zhengjie.modules.opl.domain.ServiceCatalog;
 import me.zhengjie.modules.opl.domain.SubServiceCatalog;
 import me.zhengjie.modules.opl.service.dto.CatalogCriteria;
+import me.zhengjie.modules.opl.service.dto.SubCatalogVo;
 
 
 import java.util.Map;
@@ -75,6 +76,17 @@ public interface ServiceCatalogService {
     void insertSubCatalog(CatalogCriteria catalogCriteria);
 
     /**
+     * @title: updateSubCatalog2
+     * @description: 修改服务子分类
+     * @date: 2021/2/23 18:25
+     * @author: ming.cao
+     * @param catalogCriteria
+     * @return void
+     * @throws
+     */
+    void updateSubCatalog2(CatalogCriteria catalogCriteria);
+
+    /**
      * @title: updateParentCatalog
      * @description: 修改服务父分类
      * @date: 2021/1/20 18:55
@@ -96,5 +108,28 @@ public interface ServiceCatalogService {
      * @throws
      */
     void updateSubCatalog(SubServiceCatalog subServiceCatalog);
+
+    /**
+     * @title: deleteSubCatalog
+     * @description: 根据subCatalogId删除
+     * @date: 2021/2/24 17:33
+     * @author: ming.cao
+     * @param subCatalogId
+     * @return void
+     * @throws
+     */
+    void deleteSubCatalog(Integer subCatalogId);
+
+
+    /**
+     * @title: findSubCatalogById
+     * @description: 根据id获取子分类数据
+     * @date: 2021/2/24 11:37
+     * @author: ming.cao
+     * @param catalogId
+     * @return me.zhengjie.modules.opl.service.dto.CatalogCriteria
+     * @throws
+     */
+    Map<String,Object> findSubCatalogById(Integer catalogId);
 
 }
